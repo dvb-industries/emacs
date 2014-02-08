fc59cc3a39dfd6df9b856eb60a9dd922f361afb2
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2014-02-08 09:40:02 +0100

    use destination

--- attributes/emacs.rb
+++ attributes/emacs.rb
@@ -1,4 +1,4 @@
-set_unless[:emacsd][:location] = "/home/dvberkel/.emacs.d"
+set_unless[:emacsd][:destination] = "/home/dvberkel/.emacs.d"
 set_unless[:emacsd][:repository] = "https://github.com/dvb-industries/.emacs.d.git"
 set_unless[:emacsd][:user] = "dvberkel"
 set_unless[:emacsd][:group] = "dvberkel"
--- recipes/default.rb
+++ recipes/default.rb
@@ -10,8 +10,9 @@ execute "install emacs" do
   command "apt-get -y install emacs24"
 end
 
-git node[:emacsd][:location] do
+git "~/.emacs.d" do
   repository node[:emacsd][:repository]
+  destination node[:emacsd][:destination]
   user node[:emacsd][:user]
   group node[:emacsd][:group]
 end
